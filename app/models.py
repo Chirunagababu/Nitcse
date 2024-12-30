f7375c721b58f1929e4006d370ee3d31a4827b11
Chirunagababu <jallinagababu292@gmail.com>
2024-12-30 10:34:26 +0530

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+class order(models.Model):
+	item=models.CharField(max_length=12)
+	price=models.CharField(max_length=150)
+	qn=models.IntegerField(default=10)
+
+	class student(models.Model):
+		uname=models.CharField(max_length=20)
+		fname=models.CharField(max_length=30)
+		Iname=models.CharField(max_length=30)
+		mobile=models.CharField(max_length=10)
+		email=models.EmailField()
+
+         
---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def wel(request):
+	return HttpResponse("kjgkfjhgkrdgh")
+
+def nav(self):
+	return HttpResponse("<h2>hii akhil</h2>")
+
